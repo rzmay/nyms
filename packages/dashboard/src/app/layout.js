0d44c4a3051a5ee865be576581de1bbcaf97070c
rzmay <robertmay2003@gmail.com>
2024-07-08 01:43:39 -0400

    SVH please save me

--- packages/dashboard/src/app/layout.js
+++ packages/dashboard/src/app/layout.js
@@ -20,7 +20,7 @@ export default function Layout({ children }) {
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </Head>
-      <body className="bg-null w-screen h-screen">
+      <body className="bg-null">
         <p className="absolute ml-5 mt-2 font-karnak text-4xl drop-shadow-md text-white z-50">nyms</p>
         {children}
       </body>
